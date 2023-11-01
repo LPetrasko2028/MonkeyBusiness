86966febb5853456a7f09e367976da6fc08f12e1
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-11-01 15:40:29 -0500

    Add files via upload

--- Monkey Business/client/components/Home/HomePage.jsx
+++ Monkey Business/client/components/Home/HomePage.jsx
@@ -1,43 +1,23 @@
 import React from 'react'
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-
-// import { useNavigation } from 'react-navigation'
-
-export default function Home (props) {
-  console.log('Home props: ', props)
-  console.log('Home.theme prop type: ', typeof(props.theme))
-
-  // const navigation = useNavigation()
-  // General data? Stocks notifications etc.
+import PropTypes from 'prop-types'
+function Home (props) {
+  const { name } = props
+  let content
+  if (name !== '') {
+    content = (<div> Welcome {name} </div>)
+  } else {
+    content = (<div> Home page placeholder</div>)
+  }
   return (
   <React.Fragment>
-    <div background-color= {props.theme}> Home page placeholder
-    <div className="container">
-      <table className={`table table-${props.theme} table-striped`}>
-        <thead>
-          <tr>
-            <th scope="col">Stock</th>
-            <th scope="col">Price</th>
-            <th scope="col">Change</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Apple</td>
-          <td>123</td>
-          <td>123</td>
-        </tr>
-        </tbody>
-        </table>
-        </div>
-    </div>
-    <div>
-      <tHome></tHome>
-    </div>
+    {content}
   </React.Fragment>
   )
 }
-Home.defaultProps = {
-  theme: 'light'
+Home.propTypes = {
+  name: PropTypes.string
 }
-
+Home.defaultProps = {
+  name: ''
+}
+export default Home
