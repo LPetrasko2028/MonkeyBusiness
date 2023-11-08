37e844ebecd7540d987176d79add7f8ad19d50e6
Noisymedal <noisymedal95@gmail.com>
2023-11-08 15:24:13 -0600

    comments
    updated comments on all files

--- Monkey Business/python/GetPortfolioSize.py
+++ Monkey Business/python/GetPortfolioSize.py
@@ -1,14 +1,17 @@
 import yfinance as yf
 import random
-import GetClosingPriceList as g
+import sys  # added after
+# import GetClosingPriceList as g
 
-#input: the shortname of the stock
+input= sys.argv[1]
+
+#input: list[string], the list of stock shortnames such as "AMZN" or "MSFT"
 def getPortfolio(input):
     pricelist=[]
     for i in range(len(input)):
         name=input[i]
         stock=yf.Ticker(name)
-        pricelist.append(stock.info['currentPrice'])
+        pricelist.append(stock.fast_info['lastPrice'])
     stocklist2=[]
     pricelist2 = []
     temp=0
@@ -17,11 +20,12 @@ def getPortfolio(input):
         temp=pricelist.index(max(pricelist))
         stocklist2.append(input[temp])
         pricelist2.append(pricelist[temp])
+        pricelist.remove(pricelist[temp])
         input.remove(input[temp])
-        pricelist.remove(input[temp])
     return [stocklist2,pricelist2]
 
-testlist=['AMZN', 'MSFT', 'AAPL', 'GOOG']
-print(getPortfolio(testlist))
+print(getPortfolio(input))
+# testlist=['AMZN', 'MSFT', 'AAPL', 'GOOG']
+# print(getPortfolio(testlist))
 
     
--- Monkey Business/python/MPosToChoice.py
+++ Monkey Business/python/MPosToChoice.py
@@ -1,10 +1,23 @@
 import yfinance as yf
 import random
+import sys
 
+#This file is the function that takes the monkey tracking input value "X" position and translate that to the stock choice.
 
+inputList= sys.argv[1]
+quantity= sys.argv[2]
+X= sys.argv[3]
+screenWidth= sys.argv[4]
 
+#This function takes in a list of stocks from the user, and displays a subset of them for the monkeyChoice to choose between
+#inputList, list[string], a list of stock shortnames such as "AMZN" or "MSFT", if this value is zero the function will return an error
+#quantity, int, the number of stocks the monkey is to choose between, if this value is greater than the input list, the function will return an error
+#this function returns a list of stock shortnames, culled to the quan tity specified, randomly
 def choicePicker(inputList, quantity):
     output=[]
+    if(len(inputList)<=0):
+        output="error, input stock list non-positive"
+        return output
     if (quantity<=0):
         output=["error, non-positive request size"]
         return output
@@ -20,12 +33,20 @@ def choicePicker(inputList, quantity):
             #print(output)
     return output
 
+#this function is the actual monkey choice function
+#inputList, list[string], a list of stock shortnames, such as "AMZN" or "MSFT"
+#x, double, the x-position of the monkey on-screen
+#screenwidth, int, the width of the screen upon which the monkey is being tracked
+
 def monkeyChoice(inputList,X,screenWidth):
     choice=0
     for i in range(len(inputList)):
         if X > i*screenWidth/len(inputList) and X <= (i+1)*screenWidth/len(inputList):
             choice=i
     return inputList[choice]
+
+
+print(monkeyChoice(choicePicker(inputList,quantity),X,screenWidth))
 
 
 #TESTING
@@ -40,9 +61,7 @@ def monkeyChoice(inputList,X,screenWidth):
 #                 blah=""
 #                 blah=blah+listB[i]+listB[j]+listB[k]
 #                 listD.append(blah)
-
-
-# choiceList=choicePicker(listD,300)
+# choiceList=choicePicker(listB,3)
 # print(choiceList)
-# print(monkeyChoice(choiceList,400,1080))
+# print(monkeyChoice(choiceList,30.5,1080))
 #print(listD)
