77a7cbbb9fa8594431b0237de897051cea3d0041
Noisymedal <noisymedal95@gmail.com>
2023-12-13 15:38:01 -0600

    russel100 mode
    the function now operates on the top 100 rather than a full 1000, the change can be undone easily by changing one line.

--- Monkey Business/python/Russel1000.py
+++ Monkey Business/python/Russel1000.py
@@ -1,11 +1,11 @@
 import random
 import sys
 
-quant = int(sys.argv[1])
-
+# quant = int(sys.argv[1])
+quant=100
 def reccomendStocks(quant):
 
-    russel=["A","AA","AAL","AAP","AAPL","ABBV","ABNB","ABT","ACGL","ACHC","ACI","ACM","ACN","ADBE","ADC","ADI","ADM","ADP","ADSK","ADT","AEE","AEP","AES","AFG","AFL","AFRM","AGCO","AGL","AGNC","AGO","AGR","AIG","AIRC","AIZ","AJG","AKAM","AL","ALB","ALGM","ALGN","ALK","ALL","ALLE","ALNY","ALSN","AM","AMAT","AMBP","AMC","AMCR","AMD","AME","AMED","AMG","AMGN","AMH","AMP","AMT","AMZN","AN","ANET","ANSS","AON","AOS","APA","APD","APH","APLS","APO","APP","APTV","AR","ARE","ARES","ARMK","ARW","ASH","ATO","ATR","AVB","AVGO","AVT","AVTR","AVY","AWI","AWK","AXON","AXP","AXS","AXTA","AYI","AYX","AZEK","AZO","AZPN","AZTA","BA","BAC","BAH"]
+    russel1000=["A","AA","AAL","AAP","AAPL","ABBV","ABNB","ABT","ACGL","ACHC","ACI","ACM","ACN","ADBE","ADC","ADI","ADM","ADP","ADSK","ADT","AEE","AEP","AES","AFG","AFL","AFRM","AGCO","AGL","AGNC","AGO","AGR","AIG","AIRC","AIZ","AJG","AKAM","AL","ALB","ALGM","ALGN","ALK","ALL","ALLE","ALNY","ALSN","AM","AMAT","AMBP","AMC","AMCR","AMD","AME","AMED","AMG","AMGN","AMH","AMP","AMT","AMZN","AN","ANET","ANSS","AON","AOS","APA","APD","APH","APLS","APO","APP","APTV","AR","ARE","ARES","ARMK","ARW","ASH","ATO","ATR","AVB","AVGO","AVT","AVTR","AVY","AWI","AWK","AXON","AXP","AXS","AXTA","AYI","AYX","AZEK","AZO","AZPN","AZTA","BA","BAC","BAH"]
     list2=["BALL","BAX","BBWI","BBY","BC","BDX","BEN","BEPC","BERY","BF.A","BFAM","BF.B","BG","BHF","BIIB","BILL","BIO","BJ","BK","BKNG","BKR","BLD","BLDR","BLK","BMRN","BMY","BOKF","BPOP","BR","BRK.B","BRKR","BRO","BRX","BSX","BSY","BURL","BWA","BWXT","BX","BXP","BYD","C","CABE","CACC","CACI","CAG","CAH","CAR","CARR","CASY","CAT","CAVA","CB","CBOE","CBRE","CBSH","CC","CCCS","CCI","CCK","CCL","CDAY","CDNS","CDW","CE","CEG","CELH","CERT","CF","CFG","CFLT","CFR","CG","CGNX","CHD","CHDN","CHE","CHH","CHK","CHPT","CHRW","CHTR","CI","CIEN","CINF","CL","CLF","CLH","CLVT","CLX","CMA","CMCSA","CME","CMG","CMI","CMS","CNA","CNC","CNHI","CNM"]
     list3=["CNP","CNXC","COF","COHR","COIN","COLB","COLD","COLM","COO","COP","COR","COST","COTY","CPB","CPNG","CPRI","CPRT","CPT","CR","CRI","CRL","CRM","CROX","CRUS","CRWD","CSCO","CGSP","CSL","CSX","CTAS","CTLT","CTRA","CTSH","CTVA","CUBE","CUZ","CVS","CVX","CW","CWEN","CWE.A","CXT","CZR","D","DAL","DAR","DASH","DBX","DCI","DD","DDOG","DE","DECK","DFS","DG","DGX","DHI","DHR","DINO","DIS","DISH","DKNG","DKS","DLB","DLR","DLTR","DNA","DNB","DOCS","DOCU","DOV","DOW","DOX","DPZ","DRI","DRVN","DT","DTE","DTM","DUK","DV","DVA","DVN","DXC","DXC","DXCM","EA","EBAY","ECL","ED","EEFT","EFX","EG","EGP","EHC","ETX","EL","ELAN","ELS","ELV","EME"]
     list4=["EMN","EMR","ENOV","ENPH","ENTG","EOG","EPAM","EPR","EQH","EQIX","EQR","EQT","ES","ESAB","ESI","ESS","ESCT","ETN","ETR","ETSY","EVR","EVRG","EW","EWBC","EXAS","EXC","EXEL","EXP","EXPD","EXPE","EXR","F","FAF","FANG","FAST","FBIN","FCN","FCNCA","FCX","FDS","FE","FERG","FFIV","FHB","FI","FICO","FIS","FITB","FIVE","FLO","FLS","FLT","FMC","FNB","FND","FNF","FOUR","FOX","FOXA","FR","FRPT","FRT","FSLR","FTI","FTNT","FTRE","FTV","FWONA","FWONK","FYBR","G","GD","GDDY","GE","GEHC","GEN","GFS","GGG","GILD","GIS","GL","GLOB","GLPI","GLW","GM","GME","GMED","GNTX","GO","GOOG","GOOGL","GPC","GPK","GPN","GPS","GRMN"]
@@ -16,10 +16,14 @@ def reccomendStocks(quant):
     list9=["SEE","SEIC","SF","SGEN","SHC","SHW","SIRI","SITE","SJM","SKX","SLB","SLGN","SLM","SMAR","SMG","SNA","SNDR","SNOW","SNPS","SNV","SNX","SO","SOFI","SON","SPB","SPG","SPGI","SPLK","SPOT","SPR","SQ","SRC","SRCL","SRE","SRPT","SSNC","SSRM","ST","STAG","STE","STLD","STT","STWD","STZ","SUI","SWAV","SWK","SWKS","SWN","SYF","SYK","SYY","T","TAP","TDC","TDG","TDOC","TDY","TEAM","TECH","TER","TFC","TFSL","TFX","TGT","THC","THG","THO","TJX","TKO","TKR","TMO","TMUS","TNDM","TNL","TOL","TOST","TPG","TPL","TPR","TPX","TREX","TRGP","TRIP","TRMB","TROW","TRU","TRV","TSCO","TSLA","TSN","TT","TTC","TTD","TTEK","TTWO","TW","TWLO","TXG","TXN"]
     list10=["TXRH","TXT","TYL","U","UA","UAA","UAL","UBER","UDR","UGI","UHAL","UHA.B","UHS","UI","ULTA","UNH","UNM","UNP","UPS","URI","USB","USFD","UTHR","UWMC","V","VAC","VEEV","VFC","VICI","VIRT","VLO","VLTO","VMC","VMI","VNO","VNT","VOYA","VRSK","VRSN","VRT","VRTX","VSAT","VSCO","VST","VSTS","VTR","VTRS","VVV","VYX","VZ","W","WAB","WAL","WAT","WBA","WBD","WBS","WCC","WDAY","WDC","WEC","WELL","WEN","WEX","WFC","WH","WHR","WING","WLK","WM","WMB","WMS","WMT","WOLF","WOOF","WPC","WRB","WRK","WSC","WSM","WSO","WST","WTFC","WTM","WTRG","WTW","WU","WWD","WY","WYNN","X","XEL","XOM","XP","XPO","XRAY","XYL","YETI","YUM","Z","ZBH","ZBRA","ZG","ZI","ZION","ZM","ZS","ZTS"]
 
-    russel+=list2+list3+list4+list5+list6+list7+list8+list9+list10
+    russel1000+=list2+list3+list4+list5+list6+list7+list8+list9+list10
+
     
+    russel100=["AAPL","MSFT","GOOG","GOOGL","AMZN","NVDA","TSLA","META","BRK.B","V","JPM","UNH","XOM","LLY","WMT","JNJ","MA","AVGO","PG","HD","ORCL","CVX","MRK","KO","PEP","COST","ABBV","COST","ABBV","ABDE","BAC","CRM","MCD","CSCO","PFE","TMO","ACN","NFLX","ABT","LIN","AMD","DHR","CMCSA","MKE","TMUS","DIS","WFC","TXN","UPS","PM","NEE","MS","RTX","QCOM","COP","SPGI","HON","INTU","LOW","INTC","VZ","BA","BMY","CAT","AXP","UNP","IBM","GE","DE","LMT","MDT","AMGN","SBUX","ISRG","AMAT","NOW","PLD","SYK","T","GS","BLK","SCHW","BKNG","ADI","ELV","MDLZ","TJX","GILD","ADP","MMC","CVS","AMT","C","ARTX","UBER","LRCX","SLB","CI","MO","HCA","ETN","PYPL"]
     # print(russel)
-    #
+
+    
+    input=russel100 #change THIS variable to switch to the 1000 mode
     output=[]
     if (quant>=100):
         quant=100
@@ -27,9 +31,9 @@ def reccomendStocks(quant):
         quant=0
     # i=0
     while(len(output)<quant):
-        temp=random.randint(1,999)
-        if (russel[temp] not in output):
-            output.append(russel[temp])
+        temp=random.randint(0,len(input)-1)
+        if (input[temp] not in output):
+            output.append(input[temp])
             # i+=1
     return output
 
